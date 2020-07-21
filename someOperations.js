c3a511fac4b9de05a75a123a615b1114d11e3a15
Jose Cuevas <josecueram@gmail.com>
2020-07-20 21:35:31 -0500

    Added some operations examples

---
+++ someOperations.js
@@ -0,0 +1,60 @@
+const name = 'Jose'
+const age = 24
+const friendName = 'Luis'
+const friendAge = 17
+
+// console.log(name == friendName)
+// console.log(age > friendAge)
+
+// if ( friendName === 'Jose' ) {
+//   console.log('Hello José')
+// } else {
+//   console.log('hello Luis')
+// }
+
+// if ( friendAge >= 18 ) {
+//   console.log('You can drink a beer')
+// } else {
+//   console.log('You can\'t have a beer')
+// }
+
+// const money = 200
+
+// if ( money <= 50) {
+//   console.log('you can buy some chips')
+// } else if (money <= 80) {
+//   console.log('you can buy two esquites')
+// } else if (money <= 100) {
+//   console.log('you can buy three esquites')
+// } else {
+//   console.log(' You can buy all the esquites')
+// }
+
+// const option = 5
+
+// switch (option) {
+//   case 1:
+//     console.log('Burger')
+//     break
+//   case 2:
+//     console.log('Burger & fries')
+//     break
+//   case 3:
+//     console.log('Burger, fries & soda')
+//     break
+//   default:
+//     console.log('Sorry, we don\'t have that :(')
+//     break
+// }
+
+
+for ( let i = 0; i < 10; i-- ) {
+  console.log('hello')
+}
+
+let i = 0
+while (i < 10) {
+  console.log('hello')
+  i++
+}
+
