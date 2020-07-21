1ce981bf478c955bc94563d61dab0cb6cdc344a2
Jose Cuevas <josecueram@gmail.com>
2020-07-20 23:04:27 -0500

    Added more examples

--- dataTypes.js
+++ dataTypes.js
@@ -1,3 +1,4 @@
+// ? Primitives
 // * String
 const name = 'Jose'
 const lastname = 'Cuevas'
@@ -7,3 +8,49 @@ let age = 24
 
 // * Boolean
 const likesEsquites = true
+
+// ? Objects
+// * Array
+const friendsAges = [ 24, 18, 19, 33 ]
+const friendsNames = ['José', 'Luis', 'Diego', 'Memo']
+const esquitesLikes = [true, false, true, true]
+
+// * Function
+function hello (name, age) {
+  console.log(`Hello my name is ${name} and next year im going to be ${age + 1} years old!`)
+}
+
+// * Objects
+const person = {
+  name: 'Diego',
+  age: 19,
+  likesEsquites: true,
+  greeting: function (friendName) {
+    console.log(`Hello ${friendName}`)
+  },
+  favMovies: [ 'Gladiator', 'Butterfly Effect', 'The Matrix' ],
+  dog: {
+    name: 'Lucho',
+    age: 2,
+    bark: function () {
+      console.log('woof woof!')
+    }
+  },
+  favVideogames: [
+    {
+      name: 'Halo',
+      rating: 'T',
+      console: 'Xbox'
+    },
+    {
+      name: 'God of War',
+      rating: 'M',
+      console: 'PS2'
+    },
+    {
+      name: 'Minecraft',
+      rating: 'E',
+      console: 'Multiplatform'
+    }
+  ]
+}
--- someOperations.js
+++ someOperations.js
@@ -48,13 +48,44 @@ const friendAge = 17
 // }
 
 
-for ( let i = 0; i < 10; i-- ) {
-  console.log('hello')
+// for ( let i = 0; i < 10; i-- ) {
+//   console.log('hello')
+// }
+
+// let i = 0
+// while (i < 10) {
+//   console.log('hello')
+//   i++
+// }
+
+const friendsAges = [ 24, 18, 19, 33, 18, 57, 86, 72, 4, 5, 7, 3, 10 ]
+for (let i = 0; i < friendsAges.length; i += 2 ) {
+  // i = 0
+  // i = 2
+  // i = 4
+  // i = 6
+  console.log(friendsAges[i])
 }
 
-let i = 0
-while (i < 10) {
-  console.log('hello')
-  i++
+const friendsNames = ['José', 'Luis', 'Diego', 'Memo']
+for (let i = friendsNames.length - 1; i >= 0 ; i--) {
+  // i = 4
+  // i = 3
+  // i = 2
+  // i = 1
+  // i = 0
+  console.log(friendsNames[i])
 }
+
+const numbers = []
+for (let i = 100; i >= 1; i--) {
+  numbers.push(i)
+}
+console.log(numbers)
+
+for (let i = 0; i < 100; i++) {
+  numbers.pop()
+}
+
+
 
