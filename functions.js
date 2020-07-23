d8811f8de8bc51049efc0fc77c9e7d16b5cead11
Jose Cuevas <josecueram@gmail.com>
2020-07-22 22:21:11 -0500

    Added more functions examples 🔥

--- functions.js
+++ functions.js
@@ -10,7 +10,7 @@ console.log('Area:', area)
 console.log(name)
 const name = 'Luis'
 
-
+// * Hoisting
 function sumOfNumbers (number1, number2) {
   const sum = number1 + number2
   return sum
@@ -36,3 +36,73 @@ function arrayLister ( stuff = [] ) {
 
 function objectLister ( person = {}) {
 }
+
+const money = [50, 68, 23, 1, 54, 68, 90, 45]
+//             0   1   2   3  4   5   6   7
+//            length: 8
+
+function moneyMultiplier (inputArray) {
+  const multiplier = 2
+  const newMoney = []
+ 
+  for (let i = 0; i < inputArray.length; i++) {
+    newMoney.push(inputArray[i] * multiplier)
+  }
+
+  return newMoney
+}
+
+const multipliedMoney = moneyMultiplier(money)
+console.log(multipliedMoney)
+
+
+// Jose -> ESOJ4@gmail.com
+
+function emailGenerator (name) {
+  
+  function stringInverter (string) {
+    // string -> 'D I E G O'
+    //            0 1 2 3 4
+    // length: 5
+    const auxArray = []
+    for (let i = string.length - 1; i >= 0 ; i--) {
+      // i = 4
+      // ...
+      // i = 0
+      auxArray.push(string[i])
+    }
+    // auxArray -> ["O", "G", "E", "I", "D"] ✅
+    return auxArray
+  }
+
+  function numberAdder (array) {
+    const auxArray = array
+    const number = array.length
+    const numberString = number.toString()
+    
+    auxArray.push(numberString)
+    // auxArray -> ["O", "G", "E", "I", "D", "5"] ✅
+    return auxArray
+  }
+
+  function emailAdder (array) {
+    // array -> ["O", "G", "E", "I", "D", "5"]
+    // ["O", "G", "E", "I", "D", "5", "@gmail.com"]
+    const auxArray = array
+    auxArray.push('@gmail.com')
+    return auxArray
+  }
+
+  // ["O", "G", "E", "I", "D"]
+  
+  const upperCaseName = name.toUpperCase() 
+  const invertedName = stringInverter(upperCaseName)
+  const InvertedWithNumber = numberAdder(invertedName)
+  const invertedWithEmail = emailAdder(InvertedWithNumber)
+  const finalEmail = invertedWithEmail.join('')
+  
+  return finalEmail
+  
+}
+
+const generatedEmail = emailGenerator('Diego')
