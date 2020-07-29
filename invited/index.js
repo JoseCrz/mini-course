8707d1286f99fc93f4b217a3007cf0caf51d52b2
Jose Cuevas <josecueram@gmail.com>
2020-07-29 14:47:05 -0500

    [UPDATE] Code  logic

--- invited/index.js
+++ invited/index.js
@@ -4,24 +4,20 @@ const $button2 = document.getElementById('button2')
 
 const guests = ['Luis', 'Diego', 'Enrique', 'Javier', 'Jose', 'Memo']
 
+function checkInvited (name) {
+  for(let i = 0; i < guests.length; i++) {  
+    if (name === guests[i]) return true
+  }
+  
+  return false
+}
+
 $button.addEventListener('click', function () {
   const name = $input.value
   
-  let isInvited = false
-  
-  for(let i = 0; i < guests.length; i++) {
-    console.log(`cicle: ${i}`)
-    isInvited = name === guests[i]
-    
-    if (isInvited === true) {
-      i = 1000
-    }
-  }
+  const isInvited = checkInvited(name)
 
-  // final
-  console.log(isInvited)
-
-  if (isInvited === true) {
+  if (isInvited) {
     alert('you are invited')
   } else {
     alert('you are not invited')
